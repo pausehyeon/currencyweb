5dee6a310bea4ea3ef654a74ad7259a22a25f62d
jihyeon <jihyeon@192.168.25.54>
2019-01-02 22:48:23 +0900

    initial commit

---
+++ currencyweb/src/main/java/com/pausehyeon/currencyweb/CurrencywebApplication.java
@@ -0,0 +1,14 @@
+package com.pausehyeon.currencyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencywebApplication.class, args);
+	}
+
+}
+
---
+++ currencyweb/src/test/java/com/pausehyeon/currencyweb/CurrencywebApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pausehyeon.currencyweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencywebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
